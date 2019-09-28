ac38b2e96a42382550032c76e507b8ffb3324e1f
Ramrio Vargas <jsalas2345@hotmail.com>
2019-09-28 10:07:03 -0500

    added js cong database

---
+++ src/keys.js
@@ -0,0 +1,13 @@
+  
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'database-1.cqiguf2tujb7.us-west-2.rds.amazonaws.com',
+        port:3306,
+        user: 'admin',
+        password: '12Hat931',
+        database: 'db_agro'
+    }
+
+};
